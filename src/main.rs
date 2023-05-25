942777192ef1548b13fa773deae554b247fcccee
Wongyu Lee <kyu21@outlook.com>
2023-05-25 10:48:07 -0400

    apply tls

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "수지야 젤다 좀 그만 하거라" }));
+    let app = Router::new().route("/", get(|| async { "수지야 젤다 고고" }));
 
-    axum::Server::bind(&"127.0.0.1:3000".parse().unwrap())
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
